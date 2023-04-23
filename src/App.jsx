564bdfafc13392bc94b56950c2536d2eb5a4482a
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-04-23 16:30:33 +0400

    bar added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Footer from "./assets/layouts/footer";
 import Header from "./assets/layouts/header";
 import HomePage from "./pages/home";
 import GameCenter from "./pages/gameCenter";
+import Bar from "./assets/components/bar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Bar />
 
       <Routes>
         <Route path="/gamecenter" element={<GameCenter />} />

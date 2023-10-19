ce51e379e555c7d5de79e1bd83dfa8b355f52bcb
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-10-20 00:36:38 +0400

    skills and projects

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,9 +17,17 @@ import {
 import myImage from "../../public/myImage.png";
 import myImage1 from "../../public/myImage1.jpg";
 import aboutMeAnimatedImage from "../../public/aboutMeAnimatedImage.png";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import html from "../../public/skills/html.png";
+import css from "../../public/skills/css.png";
+import typescript from "../../public/skills/typescript.png";
+import nodejs from "../../public/skills/nodejs.svg";
+import react from "../../public/skills/react.svg";
+import express from "../../public/skills/express.svg";
+import project1 from "../../public/project1.jpg";
+import project2 from "../../public/project2.jpg";
 
 export default function Home() {
   useEffect(() => {
@@ -39,6 +47,40 @@ export default function Home() {
       });
     };
   }, []);
+
+  const [experience, setExperience] = useState(0);
+  const [project, setProject] = useState(0);
+  const [client, setClient] = useState(0);
+
+  useEffect(() => {
+    const increaseExperience = () => {
+      if (experience < 2) {
+        setExperience(experience + 1);
+      }
+    };
+
+    const increaseProject = () => {
+      if (project < 10) {
+        setProject(project + 1);
+      }
+    };
+
+    const increaseClient = () => {
+      if (client < 100) {
+        setClient(client + 1);
+      }
+    };
+
+    const experienceTimer = setInterval(increaseExperience, 100);
+    const projectTimer = setInterval(increaseProject, 100);
+    const clientTimer = setInterval(increaseClient, 100);
+
+    return () => {
+      clearInterval(experienceTimer);
+      clearInterval(projectTimer);
+      clearInterval(clientTimer);
+    };
+  }, [experience, project, client]);
 
   return (
     <main className={styles.homePage}>
@@ -69,17 +111,17 @@ export default function Home() {
           <Image src={myImage} alt="myImage" priority />
           <div className={styles.experience}>
             <div>
-              <p className={styles.number}>2+</p>
+              <p className={styles.number}>{experience}+</p>
               <p>Years Of Experience</p>
             </div>
             <hr />
             <div>
-              <p className={styles.number}>10+</p>
+              <p className={styles.number}>{project}+</p>
               <p>Project Complete</p>
             </div>
             <hr />
             <div>
-              <p className={styles.number}>100%</p>
+              <p className={styles.number}>{client}%</p>
               <p>Client Satisfactions</p>
             </div>
           </div>
@@ -177,7 +219,6 @@ export default function Home() {
       </section>
 
       {/* MY RESUME */}
-
       <section className={styles.myResumeSection}>
         <div className={styles.container}>
           <div className={styles.flex}>
@@ -245,6 +286,231 @@ export default function Home() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+      {/* POPULAR SERVICES */}
+      <section className={styles.popularServicesSection}>
+        <div className={styles.container}>
+          <div className={styles.sectionName}>
+            <h5>Popular Services</h5>
+          </div>
+
+          <div className={styles.headText} data-aos="zoom-out-up">
+            <h1>
+              My <span>Special Service</span> For your Business Development
+            </h1>
+          </div>
+
+          <div className={styles.cards}>
+            <div className={styles.card} data-aos="zoom-out-right">
+              <div className={styles.number}>01.</div>
+              <div className={styles.serviceName}>
+                <h2>Brand Identity Design</h2>
+                <p>Dignissimos ducimus blanditiis praesen</p>
+              </div>
+              <div className={styles.icon}>
+                <FontAwesomeIcon icon={faLocationArrow} />
+              </div>
+            </div>
+            <div className={styles.card} data-aos="zoom-out-left">
+              <div className={styles.number}>02.</div>
+              <div className={styles.serviceName}>
+                <h2>Brand Identity Design</h2>
+                <p>Dignissimos ducimus blanditiis praesen</p>
+              </div>
+              <div className={styles.icon}>
+                <FontAwesomeIcon icon={faLocationArrow} />
+              </div>
+            </div>
+            <div className={styles.card} data-aos="zoom-out-right">
+              <div className={styles.number}>03.</div>
+              <div className={styles.serviceName}>
+                <h2>Brand Identity Design</h2>
+                <p>Dignissimos ducimus blanditiis praesen</p>
+              </div>
+              <div className={styles.icon}>
+                <FontAwesomeIcon icon={faLocationArrow} />
+              </div>
+            </div>
+            <div className={styles.card} data-aos="zoom-out-left">
+              <div className={styles.number}>04.</div>
+              <div className={styles.serviceName}>
+                <h2>Brand Identity Design</h2>
+                <p>Dignissimos ducimus blanditiis praesen</p>
+              </div>
+              <div className={styles.icon}>
+                <FontAwesomeIcon icon={faLocationArrow} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* MY SKILLS */}
+      <section className={styles.mySkills}>
+        <div className={styles.container}>
+          <div className={styles.left} data-aos="fade-down-right">
+            <div className={styles.sectionName}>
+              <h5>My Skills</h5>
+            </div>
+            <div className="headText">
+              <h1>
+                Let’s Explore Popular <span>Skills & Experience</span>
+              </h1>
+
+              <p>
+                Sed ut perspiciatis unde omnis iste natus to voluptatem
+                accusantium doloremque laudantium, totam rem aperiamc eaque ipsa
+                quae ab illo inventore veritatis
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.rightSkills}>
+            <div className={styles.skills} data-aos="fade-down-left">
+              <div className={styles.skill}>
+                <div className={styles.skillLogo}>
+                  <Image src={html} alt="HTML" />
+                </div>
+                <div className={styles.skillName}>
+                  <h4>Html</h4>
+                </div>
+                <div className={styles.skillPercentage}>100%</div>
+              </div>
+
+              <div className={styles.skill}>
+                <div className={styles.skillLogo}>
+                  <Image src={css} alt="CSS" />
+                </div>
+                <div className={styles.skillName}>
+                  <h4>Css</h4>
+                </div>
+                <div className={styles.skillPercentage}>100%</div>
+              </div>
+
+              <div className={styles.skill}>
+                <div className={styles.skillLogo}>
+                  <Image src={typescript} alt="typescript" />
+                </div>
+                <div className={styles.skillName}>
+                  <h4>TypeScript</h4>
+                </div>
+                <div className={styles.skillPercentage}>85%</div>
+              </div>
+
+              <div className={styles.skill}>
+                <div className={styles.skillLogo}>
+                  <Image src={react} alt="REACT" />
+                </div>
+                <div className={styles.skillName}>
+                  <h4>React</h4>
+                </div>
+                <div className={styles.skillPercentage}>95%</div>
+              </div>
+
+              <div className={styles.skill}>
+                <div className={styles.skillLogo}>
+                  <Image src={express} alt="EXPRESS.JS" />
+                </div>
+                <div className={styles.skillName}>
+                  <h4>Express.js</h4>
+                </div>
+                <div className={styles.skillPercentage}>100%</div>
+              </div>
+
+              <div className={styles.skill}>
+                <div className={styles.skillLogo}>
+                  <Image src={nodejs} alt="NODE.JS" />
+                </div>
+                <div className={styles.skillName}>
+                  <h4>Node.js</h4>
+                </div>
+                <div className={styles.skillPercentage}>95%</div>
+              </div>
+
+              <div className={styles.skill}>
+                <div className={styles.skillLogo}>
+                  <Image src={html} alt="NEXT.JS" />
+                </div>
+                <div className={styles.skillName}>
+                  <h4>Next.js</h4>
+                </div>
+                <div className={styles.skillPercentage}>90%</div>
+              </div>
+
+              <div className={styles.skill}>
+                <div className={styles.skillLogo}>
+                  <Image src={html} alt="JAVASCRIPT" />
+                </div>
+                <div className={styles.skillName}>
+                  <h4>JavaScript</h4>
+                </div>
+                <div className={styles.skillPercentage}>90%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* LATEST WORKS */}
+      <section className={styles.latestWorksSection}>
+        <div className={styles.container}>
+          <div className={styles.sectionName}>
+            <h5>Latest Works</h5>
+          </div>
+          <div className={styles.headText}>
+            <h1>
+              Explore My Popular <span>Projects</span>
+            </h1>
+          </div>
+
+          <div className={styles.projects}>
+            <div className={styles.project}>
+              <div className={styles.image} data-aos="fade-right">
+                <Image src={project1} alt="Project 1" />
+              </div>
+              <div className={styles.projectDescription} data-aos="fade-left">
+                <b>FullStack Builder</b>
+                <Link href={"/projectDetail"}>WebSite Builder</Link>
+                <p>
+                  Sed ut perspiciatis unde omnin natus totam rem aperiam eaque
+                  inventore veritatis architecto beatae
+                </p>
+
+                <div className={styles.icon}>
+                  <Link href={"/projectDetail"}>
+                    <FontAwesomeIcon icon={faLocationArrow} />
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className={styles.project}>
+              <div className={styles.projectDescription} data-aos="fade-right">
+                <b>FullStack Builder</b>
+                <Link href={"/projectDetail"}>WebSite Builder</Link>
+                <p>
+                  Sed ut perspiciatis unde omnin natus totam rem aperiam eaque
+                  inventore veritatis architecto beatae
+                </p>
+
+                <div className={styles.icon}>
+                  <Link href={"/projectDetail"}>
+                    <FontAwesomeIcon icon={faLocationArrow} />
+                  </Link>
+                </div>
+              </div>
+              <div className={styles.image} data-aos="fade-left">
+                <Image src={project2} alt="Project 2" />
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.viewMoreButton}>
+            <Link href={"/allProjects"} className={styles.btn}>
+              View More Projects <FontAwesomeIcon icon={faAngleRight} />
+            </Link>
           </div>
         </div>
       </section>
